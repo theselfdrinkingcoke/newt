cc48b3e5a8d8390ce100c0c710b342678544c3bc
theselfdrinkingcoke <204591683+theselfdrinkingcoke@users.noreply.github.com>
2026-02-01 11:57:38 -0600

    error handling

--- newt.c
+++ newt.c
@@ -1,39 +1,76 @@
+/*** includes ***/
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
+
 // Stores original state of the terminal
 struct termios orig_termios;
 
+
+/*** terminal ***/
+
+// Prints an error and kills the program
+void kill(const char *s) {
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+        kill("tcsetattr");
+    }
 }
 
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+        kill("tcgetattr");
+    } 
+    
     atexit(disableRawMode);
     
     // Copy of orig_termios to make changes
     struct termios raw = orig_termios;
     
-    // Disables terminal ECHO & reads input byte by byte
-    raw.c_lflag &= ~(ECHO | ICANON);
+    // Disables carriage return/newline, flow control signals 
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    // Disables output processing
+    raw.c_oflag &= ~(OPOST);
+
+    raw.c_cflag |= (CS8);
+
+    // Disables terminal ECHO, canonical mode, terminate and suspend signals 
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+        kill("tcsetattr");
+    }
 }
+
+/*** init ***/
 
 int main() {
     enableRawMode();
-
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
-        if (iscntrl(c)) {
-            printf("%d\n", c);
-        } else {
-            printf("%d ('%c')\n", c, c);
+ 
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+            kill("read");
         }
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q') break;
     }
     
     return 0;
