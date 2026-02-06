d37e61811a674f4cb987a7f583f4a768183d8a2e
theselfdrinkingcoke <204591683+theselfdrinkingcoke@users.noreply.github.com>
2026-02-05 22:23:45 -0600

    linux kernel styling cuz why not

--- newt.c
+++ newt.c
@@ -1,4 +1,3 @@
-
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -9,97 +8,95 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 
-// Stores original state of the terminal
 struct termios orig_termios;
 
 
+void kill(const char *s) 
+{
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
 
-// Prints an error and kills the program
-void kill(const char *s) {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
-
-    perror(s);
-    exit(1);
+        perror(s);
+        exit(1);
 }
 
-void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
-        kill("tcsetattr");
-    }
-}
-
-void enableRawMode() {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
-        kill("tcgetattr");
-    } 
-    
-    atexit(disableRawMode);
-    
-    // Copy of orig_termios to make changes
-    struct termios raw = orig_termios;
-    
-    // Disables carriage return/newline, flow control signals 
-    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
-
-    // Disables output processing
-    raw.c_oflag &= ~(OPOST);
-
-    raw.c_cflag |= (CS8);
-
-    // Disables terminal ECHO, canonical mode, terminate and suspend signals 
-    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-    raw.c_cc[VMIN] = 0;
-    raw.c_cc[VTIME] = 1;
-
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
-        kill("tcsetattr");
-    }
-}
-
-char editorReadKey() {
-    int nread;
-    char c;
-    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
-        if (nread == -1 && errno != EAGAIN) {
-            kill("read");
+void disableRawMode() 
+{
+        if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+                kill("tcsetattr");
         }
-    }
-    return c;
 }
 
-void editorDrawRows() {
-    for (int i = 0; i < 24; i++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
-    }
-}
-
-void editorRefreshScreen() {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
-
-    editorDrawRows();
-    write(STDOUT_FILENO, "\x1b[H", 3);
-}
-
-void editorProcessKeypress() {
-    char c = editorReadKey();
-    switch (c) {
-        case CTRL_KEY('q'):
-            write(STDOUT_FILENO, "\x1b[2J", 4);
-            write(STDOUT_FILENO, "\x1b[H", 3);
-            exit(0);
-            break;
-    }
-}
-
-int main() {
-    enableRawMode();
- 
-    while (1) {
-        editorRefreshScreen();
-        editorProcessKeypress();
-    }
+void enableRawMode() 
+{
+        if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+                kill("tcgetattr");
+        } 
     
-    return 0;
+        atexit(disableRawMode);
+    
+        struct termios raw = orig_termios;
+    
+        raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+        raw.c_oflag &= ~(OPOST);
+        raw.c_cflag |= (CS8);
+        raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+        raw.c_cc[VMIN] = 0;
+        raw.c_cc[VTIME] = 1;
+
+        if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+                kill("tcsetattr");
+        }
+}
+
+char editorReadKey() 
+{
+        int nread;
+        char c;
+        while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+                if (nread == -1 && errno != EAGAIN) {
+                        kill("read");
+                }
+        }
+        return c;
+}
+
+void editorDrawRows() 
+{
+        for (int i = 0; i < 24; i++) {
+                write(STDOUT_FILENO, "~\r\n", 3);
+        }
+}
+
+void editorRefreshScreen() 
+{
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
+
+        editorDrawRows();
+        write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+void editorProcessKeypress() 
+{
+        char c = editorReadKey();
+        switch (c) {
+        case CTRL_KEY('q'):
+                write(STDOUT_FILENO, "\x1b[2J", 4);
+                write(STDOUT_FILENO, "\x1b[H", 3);
+                exit(0);
+                break;
+        }
+}
+
+int main() 
+{
+        enableRawMode();
+ 
+        while (1) {
+                editorRefreshScreen();
+                editorProcessKeypress();
+        }
+    
+        return 0;
 }
